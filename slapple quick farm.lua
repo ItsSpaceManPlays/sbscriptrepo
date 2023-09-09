ac2fc46ff3b84b085e959eb99e1a8c9a5592942c
ItsSpaceManPlays <80125192+ItsSpaceManPlays@users.noreply.github.com>
2023-09-09 17:07:54 +0100

    Update slapple quick farm.lua

--- slapple quick farm.lua
+++ slapple quick farm.lua
@@ -2,6 +2,53 @@ local slapGoal = slap_ammount
 local startingSlaps = slap_start
 local serverIndex = slap_index
 
+local function ReturnGui()
+    local ScreenGui = Instance.new("ScreenGui")
+    local Frame = Instance.new("Frame")
+    local TextLabel = Instance.new("TextLabel")
+    local TextLabel_2 = Instance.new("TextLabel")
+
+    ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    ScreenGui.ResetOnSpawn = false
+    ScreenGui.IgnoreGuiInset = true
+
+    Frame.Parent = ScreenGui
+    Frame.BackgroundColor3 = Color3.fromRGB(47, 47, 47)
+    Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    Frame.BorderSizePixel = 0
+    Frame.Size = UDim2.new(1, 0, 1, 0)
+
+    TextLabel.Parent = Frame
+    TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    TextLabel.BackgroundTransparency = 1.000
+    TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    TextLabel.BorderSizePixel = 0
+    TextLabel.Position = UDim2.new(0.300717145, 0, 0.423312902, 0)
+    TextLabel.Size = UDim2.new(0.398155838, 0, 0.0754601136, 0)
+    TextLabel.Font = Enum.Font.Ubuntu
+    TextLabel.Text = "0 / 500 of 1000 slaps"
+    TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+    TextLabel.TextScaled = true
+    TextLabel.TextSize = 14.000
+    TextLabel.TextWrapped = true
+    TextLabel.Name = "Stats"
+
+    TextLabel_2.Parent = Frame
+    TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    TextLabel_2.BackgroundTransparency = 1.000
+    TextLabel_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    TextLabel_2.BorderSizePixel = 0
+    TextLabel_2.Position = UDim2.new(0.311475337, 0, 0.515337467, 0)
+    TextLabel_2.Size = UDim2.new(0.376639456, 0, 0.0611451827, 0)
+    TextLabel_2.Font = Enum.Font.Ubuntu
+    TextLabel_2.Text = "Epic slap autofarm frfr"
+    TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+    TextLabel_2.TextScaled = true
+    TextLabel_2.TextSize = 14.000
+    TextLabel_2.TextWrapped = true
+    return ScreenGui
+end
+
 local function formatServerIdList()
     local serverIdTable = {}
     local success, err = pcall(function()
@@ -65,5 +112,7 @@ else
             loadstring(game:HttpGet("https://raw.githubusercontent.com/ItsSpaceManPlays/sbscriptrepo/main/slapple%20quick%20farm.lua"))()
         ]])
     end
-    game:GetService("TeleportService"):TeleportToPlaceInstance(6403373529, server, player)
+    local gui = ReturnGui()
+    gui.Frame.Stats.TextLabel.Text = currentSlaps - startingSlaps.." / "..slapGoal.." of "..startingSlaps + slapGoal.." slaps"
+    game:GetService("TeleportService"):TeleportToPlaceInstance(6403373529, server, player, nil, nil, gui)
 end
